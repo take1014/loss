4cefbecb78198eeeaefe03a87cb20702375ca77b
take1014 <y.takehara1014@gmail.com>
2021-03-31 01:53:34 +0900

    mod smooth_L1_loss

--- loss.py
+++ loss.py
@@ -3,16 +3,22 @@
 import tensorflow as tf
 
 # Smooth L1 loss function
-def smooth_L1_loss(x):
+def smooth_L1_loss(x, beta=1.0, reduction='mean'):
     '''
     x =(y_true-y_pred)
-    Both y_true and y_pred are the positive position.
+    The data must have ground truth.
     '''
     # 0.5 * (x**2)
-    mul = tf.math.multiply(0.5, tf.math.pow(x, 2.0))
+    mul = tf.math.multiply(0.5, tf.math.pow(x, 2.0)) / beta
     # |x|-0.5
-    sub = tf.math.substract(tf.math.abs(x), 0.5)
+    sub = tf.math.substract(tf.math.abs(x), 0.5) * beta
     # list of |x| < 1.0
     cond = tf.math.less(tf.math.abs(x), 1.0)
     out = tf.where(cond, mul, sub)
-    return tf.math.reduce_mean(out)
+
+    if reduction == 'sum':
+        out = tf.math.reduce_sum(out)
+    else:
+        out = tf.math.reduce_mean(out)
+
+    return out
